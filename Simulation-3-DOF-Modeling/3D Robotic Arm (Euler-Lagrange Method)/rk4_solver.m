b1216c49d2be174baf98bfc9e8dfd321b34732df
Winnie <reganwinnifred@gmail.com>
2025-04-18 03:32:32 -0600

    Update rk4 solver to have timesteps

--- Simulation-3-DOF-Modeling/3D Robotic Arm (Euler-Lagrange Method)/rk4_solver.m
+++ Simulation-3-DOF-Modeling/3D Robotic Arm (Euler-Lagrange Method)/rk4_solver.m
@@ -14,6 +14,9 @@ function [t, y] = rk4_solver(dynamics, tspan, y0, dt)
     % Initialize time vector
     t = tspan(1):dt:tspan(2);
     num_steps = length(t);
+
+    % % Store state derivatives
+    % y_dot = zeros(num_steps, length(y0));
     
     % Initialize solution matrix
     y = zeros(num_steps, length(y0));
@@ -23,9 +26,15 @@ function [t, y] = rk4_solver(dynamics, tspan, y0, dt)
     for i = 1:(num_steps-1)
         ti = t(i);       % Current time
         yi = y(i, :)';   % Current solution (as a column vector)
-        
+
+        if rem(ti , 1) == 0
+            fprintf("%i ", ti)
+        end
+ 
+        m = dynamics(ti, yi);
+
         % Compute RK4 coefficients
-        k1 = dt * dynamics(ti, yi);
+        k1 = dt * m;
         k2 = dt * dynamics(ti + dt/2, yi + k1/2);
         k3 = dt * dynamics(ti + dt/2, yi + k2/2);
         k4 = dt * dynamics(ti + dt, yi + k3);
@@ -34,4 +43,3 @@ function [t, y] = rk4_solver(dynamics, tspan, y0, dt)
         y(i+1, :) = yi' + (k1' + 2*k2' + 2*k3' + k4') / 6;
     end
 end
-
