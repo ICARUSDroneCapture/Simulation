dd4a82186cc49e0e6d865d4d102f43825aeffd92
Kirin Kawamoto <kika3780@colorado.edu>
2025-04-24 22:27:23 -0600

    Added gain mixing region coloring to piecewise scale graph

--- PiecwiseGainMixingFunctions.m
+++ PiecwiseGainMixingFunctions.m
@@ -34,7 +34,7 @@ b = 0.5;
 r_b = 0.5*b; % Zero relative position control radius
 
 % Polynomial order
-n = 4;
+n = 1;
 
 
 % --------------------------- Mixing Functions ------------------------- %
@@ -67,17 +67,28 @@ B = @(x) (a_b*abs(x-(d+sign(x-d)*r_b)).^n + k_b) .* (abs(x - d) > r_b & ...
 
 % ---------------------- Gain Mixing Visualization --------------------- %
 
-theta = linspace(d - 1.5*w/2, d + 1.5*w/2,1000); % relative position test values
+m_lim = 1.14;
+theta = linspace(d - m_lim*w/2, d + m_lim*w/2,1000); % relative position test values
 cent_gain = C(theta); % Proportion of center control gain
 boundary_gain = B(theta); % Proportion of boundary control gain
 
-% figure;
-plot(theta, cent_gain, 'LineWidth', 1)
+figure;
+fill([d-r_c d-r_c d+r_c d+r_c], [1.1 -0.1 -0.1 1.1], "r", 'FaceColor', "#b6d7a8", ...
+    'FaceAlpha', 0.4, 'EdgeColor','none');
 hold on
-text(d-0.02*w, 0, 'Boundary Control Scale', 'FontSize', 11, ...
+fill([d-w/2 d-w/2 d-r_c d-r_c], [1.1 -0.1 -0.1 1.1], "r", 'FaceColor', "#ffe59a", ...
+    'FaceAlpha', 0.4, 'EdgeColor','none');
+fill([d+w/2 d+w/2 d+r_c d+r_c], [1.1 -0.1 -0.1 1.1], "r", 'FaceColor', "#ffe59a", ...
+    'FaceAlpha', 0.4, 'EdgeColor','none');
+fill([d-m_lim*w/2 d-m_lim*w/2 d-w/2 d-w/2], [1.1 -0.1 -0.1 1.1], "r", 'FaceColor', "#ea9998", ...
+    'FaceAlpha', 0.4, 'EdgeColor','none');
+fill([d+m_lim*w/2 d+m_lim*w/2 d+w/2 d+w/2], [1.1 -0.1 -0.1 1.1], "r", 'FaceColor', "#ea9998", ...
+    'FaceAlpha', 0.4, 'EdgeColor','none');
+plot(theta, cent_gain, 'LineWidth', 1.5)
+text(d+0.2*w, 0, 'Boundary Control Scale', 'FontSize', 11, ...
     'HorizontalAlignment', 'right', 'VerticalAlignment', 'top');
-plot(theta, boundary_gain, 'LineWidth', 1)
-text(d+0.02*w, 1, 'Inertial Control Scale', 'FontSize', 11, ...
+plot(theta, boundary_gain, 'LineWidth', 1.5)
+text(d-0.18*w, 1, 'Inertial Control Scale', 'FontSize', 11, ...
     'HorizontalAlignment', 'left', 'VerticalAlignment', 'bottom');
 xline(d,'--','Label',{'Desired'; 'Position'},...
     'LabelHorizontalAlignment','right',...
@@ -87,7 +98,7 @@ xline(d,'--','Label',{'Desired'; 'Position'},...
 title('Inertial Stability and Boundary Control Mixing')
 xlabel('Relative Position')
 ylabel('Gain Proportion')
-xlim([d - 1.1*w/2 d + 1.1*w/2])
+xlim([d - m_lim*w/2 d + m_lim*w/2])
 ylim([-0.1 1.1])
 % legend('Center Control Scale','Boundary Control Scale', 'Desired Position', ...
 %        'Location','eastoutside')
